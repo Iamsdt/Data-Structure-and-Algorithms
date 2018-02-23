fa39c62eb3fd25f9850728aa49b8f7053a1bc3a0
Shudipto <Shudiptotrafder@gmail.com>
2018-02-23 07:16:26 +0600

    bubble sort

--- src/sortingAlgorithm/BubbleSort.java
+++ src/sortingAlgorithm/BubbleSort.java
@@ -1,7 +1,35 @@
 package sortingAlgorithm;
 
+import java.util.Arrays;
+
 public class BubbleSort {
 
-    public static void main(String[] arg){
+    private static void  bubbleSort(int[] array){
+        int n = array.length;
+        int temp;
+        
+        for (int i = 0; i < n ; i++) {
+            for (int j = 1; j <(n-i) ; j++) {
+                if (array[j-1] > array[j]){
+                    temp = array[j-1];
+                    array[j-1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
     }
+
+    public static void main(String[] arg){
+        int[] array = {1,4,2,10,6,8,9,3,7,5};
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("After Sort");
+
+        bubbleSort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+
+
+    // big o is n square
 }
