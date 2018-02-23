ff4740d1e4db66a32c0a8893e8adfcd1ac9b05c5
Shudipto <Shudiptotrafder@gmail.com>
2018-02-23 07:33:13 +0600

    selet ion sort

---
+++ src/sortingAlgorithm/SelectionSort.java
@@ -0,0 +1,35 @@
+package sortingAlgorithm;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+
+    private static void selectionSort(int[] array){
+        for (int i = 0; i < array.length - 1; i++) {
+            int index = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i]){
+                    index = j;
+                }
+            }
+
+            if (index != i){
+                int temp = array[index];
+                array[index] = array[i];
+                array[i] = temp;
+            }
+        }
+    }
+    
+    public static void main(String[] arg){
+
+        int[] array = {1,4,2,10,6,8,9,3,7,5};
+
+        System.out.println(Arrays.toString(array));
+
+        selectionSort(array);
+
+        System.out.println(Arrays.toString(array));
+    }
+}
