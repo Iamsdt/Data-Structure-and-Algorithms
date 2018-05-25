43e65d324b6f7b5bd4d0fa286c4380534ce79b44
Iamsdt <shudiptotrafder@gmail.com>
2018-05-25 08:07:42 +0600

    Recursion

--- src/dataStructureJava/RecursionDemo.java
+++ src/dataStructureJava/RecursionDemo.java
@@ -3,5 +3,19 @@ package dataStructureJava;
 public class RecursionDemo {
     public static void main(String[] arg) {
 
+        //it's mean a function called it's self\
+
+        //recursion use stack implement
+
+        int i = 9;
+        System.out.println("Factorial of "+i+"! is:"+fact(i));
+
+    }
+
+    private static int fact(int n) {
+        if (n == 0)
+            return 1;
+        else
+            return n * fact(n-1);
     }
 }
