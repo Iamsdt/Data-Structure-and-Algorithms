b3761ae8ad23f6ba25a24bee1514450d5fbeaf56
Shudipto <Shudiptotrafder@gmail.com>
2018-11-29 21:57:22 +0600

    re import
    Took 56 minutes

--- src/TestK.kt
+++ src/TestK.kt
@@ -1,35 +1,3 @@
-import kotlin.math.absoluteValue
-
-//fun main(args: Array<String>) {
-//    val list = listOf<String>("Shudipto","Trafder","Himu")
-//
-//
-//    val sb = StringBuilder()
-//    for (s in list){
-//        sb.append("$s,")
-//    }
-//
-//    val string = sb.toString()
-//    println("List to String:$string")
-//
-//    println("\n\n Do reverse")
-//    val newLIst = arrayListOf<String>()
-//
-//    if (string.contains(",")){
-//        val strings = string.split(",")
-//        for (li in strings){
-//            newLIst.add(li)
-//        }
-//    }
-//
-//    println("NewList $newLIst")
-//}
-
 fun main(args: Array<String>) {
-    val buget:Double = 30.45
-    val complete = 20.15
 
-    val data = (complete/buget) * 100
-
-    print(data.absoluteValue)
 }
